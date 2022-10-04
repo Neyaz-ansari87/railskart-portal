b3f39f56ccfcf94d0f36cc76fcff848095b4c965
neyaz-ansari87 <mohammadneyazkmt@gmail.com>
2022-10-04 13:33:12 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   resources :interview_reminders do
   member do
     get :done
     end
   end
-=======
-  resources :interview_reminders
->>>>>>> 59b0e8f8d6ee3f8182b2cb1767a8da9acee52db5
+
+
   resources :portals
   root 'home#index'
 
